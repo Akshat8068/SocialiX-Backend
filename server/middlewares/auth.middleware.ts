68d955c56c2b9bdf7cd77d8b7d324d2f678f16ce
Akshat <akshatyadav8068@gmail.com>
2026-07-28 18:27:38 +0530

    Access token in cookiee

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -2,7 +2,7 @@ import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import userRepositoryMethods from "../repository/user.repository.js";
 
-interface TokenPayload extends jwt.JwtPayload{
+interface TokenPayload extends jwt.JwtPayload {
   id: number;
 }
 
@@ -12,13 +12,14 @@ const authMiddleware = async (
   next: NextFunction
 ) => {
   try {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      const token = req.headers.authorization.split(" ")[1]!;
-
-      const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET!) as unknown as TokenPayload;
+      const token = req.cookies.AccessToken;
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "Access token not found",
+        });
+      }
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as unknown as TokenPayload;
 
       const user = await userRepositoryMethods.findById(decoded.id);
 
@@ -32,12 +33,7 @@ const authMiddleware = async (
       req.user = user;
 
       next();
-    } else {
-      return res.status(401).json({
-        success: false,
-        message: "Access token not found",
-      });
-    }
+    
   } catch (error) {
     return res.status(401).json({
       success: false,
